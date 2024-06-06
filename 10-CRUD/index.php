d5281af6709fd6e15d88673a07eed3c8019f92bb
ana sofia moreno <anamorenorenteria123@gmail.com>
2024-06-06 16:04:26 -0500

    feat:CRUD

--- 10-CRUD/index.php
+++ 10-CRUD/index.php
@@ -1,55 +1,3 @@
-<?php session_start();
-
-if ($_SERVER["REQUEST_METHOD"] == 'POST') {
-
-    $usuario = $_POST['user'];
-    $password = $_POST['password'];
-
-    $user_register = isset($_SESSION['userRegister']) ? $_SESSION['userRegister'] : null;
-    $pass_register = isset($_SESSION['passRegister']) ? $_SESSION['passRegister'] : null;
-
-    if (empty($usuario) or empty($password)) {
-        echo 'rellene completo el formulario';
-    } else {
-        /*  echo $usuario . ' - ' . $password;
-            if( $usuario == $user_register && $password == $pass_register){
-                echo 'listo, iniciaste sesion 👻';
-                header('location: user.php');
-            }else{  echo 'Tu usuario no existe😴';} */
-
-        //CONEXION CON LA BASE DE DATOS......
-        //entrega final
-
-
-
-        try {
-            $conexion = new PDO("mysql: host=localhost; dbname=focaapp;", 'root', '');
-            echo "conexion OK";
-        } catch (PDOException $e) {
-            echo "Error: " . $e->getMessage();
-        }
-
-        $statement = $conexion->prepare("SELECT * FROM userapp
-            WHERE username = :user AND contraseña = :pass");
-
-        $statement->execute(array(':user' => $usuario, ':pass' => $password));
-
-        $result = $statement->fetchAll();
-
-        if ($result) {
-            $_SESSION['userRegister'] = $usuario;
-            $_SESSION['passRegister'] = $password;
-            header('location:user.php');
-        } else {
-            echo 'false';
-        }
-    }
-}
-//$_SESSION['pais'] = 'colombia';
-?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -66,30 +14,26 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
         <h1 class="inicio">Pagina de inicio</h1>
 
         <form class="info" action="index.php" method="POST">
-
-        <label for="user">usuario</label>
+            <div class="usuario">
+                <label for="user">Usuario</label>
                 <br>
 
                 <input class="barrau" type="text" placeholder="usuario" name="user">
+            </div>
+            <div class="password">
+                <label for="password">Contraseña</label>
+                <br>
+                <input class="barrau" type="text" placeholder="password" name="password">
 
-                <div class="password">
-                    <label for="password">contraseña</label>
-                    <br>
-                    <input class="barrau" type="text" placeholder="password" name="password">
+            </div>
+            <button class="sesion" type="submit">Inicio sesion</button>
+            <a class="tboton" href="./registro.php">Registrate</a>
 
-                </div>
-                <button class="sesion" type="submit">Inicio sesion</button>
-                <a class="tboton" href="./registro.php">registrate</a>
+
         </form>
 
 
     </div>
-
-<video width="100%" autoplay loop muted>
-    <source src="./2235524_man_baking_making_3840x2160.mp4" type="video/mp4">
-</video>
-
-
 </body>
 
 </html>
